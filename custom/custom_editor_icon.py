7eb79c92a5726df69ea65744882848495c5b2c89
SnyWang <wangxulong@>
2023-12-09 19:57:03 +0800

    auto save : 19:57:02

--- custom/custom_editor_icon.py
+++ custom/custom_editor_icon.py
@@ -1,4 +1,5 @@
 # -*- encoding:utf-8 -*-
 _reload_all = True
 
-data = {}
+
+data = {134227427: {'path': 'ui/134227427', 'describe': '', 'name': u'image55p', 'key': 134227427, 'uid': '134227427', 'size': '457X166'}, 134223233: {'path': 'ui/134223233', 'describe': '', 'name': u'image3p', 'key': 134223233, 'uid': '134223233', 'size': '980X177'}, 134232041: {'path': 'ui/134232041', 'describe': '', 'name': u'image777p', 'key': 134232041, 'uid': '134232041', 'size': '781X177'}, 134274102: {'path': 'ui/134274102', 'describe': '', 'name': u'image2020p', 'key': 134274102, 'uid': '134274102', 'size': '1067X144'}, 134229686: {'path': 'ui/134229686', 'describe': '', 'name': u'image9999p', 'key': 134229686, 'uid': '134229686', 'size': '934X144'}}
